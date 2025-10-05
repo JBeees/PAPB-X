3412d91ec226a30c73381daf5a8346a096ab6ad5
JBeees <arifkizn9@gmail.com>
2025-10-05 22:16:23 +0700

    4

---
+++ app/src/main/java/com/example/laprak/NoteViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.laprak
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.laprak.data.model.Note
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+
+class NoteViewModel : ViewModel() {
+    private val db = FirebaseFirestore.getInstance()
+
+    private val _notes = MutableLiveData<List<Note>>()
+    val notes: LiveData<List<Note>> get() = _notes
+
+    fun fetchNotes() {
+        db.collection("notes").addSnapshotListener { snapshot, e ->
+            if (e != null || snapshot == null) return@addSnapshotListener
+            val list = snapshot.documents.mapNotNull {
+                it.toObject<Note>()?.copy(id = it.id)
+            }
+            _notes.value = list
+        }
+    }
+
+    fun addNote(note: Note) {
+        val data = note.copy(
+            timestamp = System.currentTimeMillis()
+        )
+        db.collection("notes").add(data)
+    }
+
+    fun updateNote(noteId: String, note: Note) {
+        db.collection("notes").document(noteId).set(note)
+    }
+
+    fun deleteNote(noteId: String) {
+        db.collection("notes").document(noteId).delete()
+    }
+}
---
+++ app/src/main/java/com/example/laprak/data/model/Note.kt
@@ -0,0 +1,8 @@
+package com.example.laprak.data.model
+
+data class Note(
+    val id: String? = null,
+    val title: String = "",
+    val content: String = "",
+    val timestamp: Long? = null
+)
